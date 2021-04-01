719424047280a7818a838cba3e03a3c7a31ff361
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-04-01 20:31:24 +0200

    Create SongOverview component

---
+++ src/components/SongOverview.js
@@ -0,0 +1,13 @@
+import SongForm from './SongForm';
+import SongTable from './SongTable';
+
+function SongOverview() {
+    return (
+        <div>
+            <SongForm />
+            <SongTable />
+        </div>
+    );
+}
+
+export default SongOverview;
