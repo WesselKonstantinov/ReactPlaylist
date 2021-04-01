89ecf2e2fa470406d0d5c545af567a061dfd3b01
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-04-01 19:57:19 +0200

    Create Song component

---
+++ src/components/Song.js
@@ -0,0 +1,12 @@
+function Song() {
+    return (
+        <tr>
+            <td>Rockin' For Myself</td>
+            <td>Motiv8</td>
+            <td>Eurodance</td>
+            <td>5</td>
+        </tr>
+    );
+}
+
+export default Song;
